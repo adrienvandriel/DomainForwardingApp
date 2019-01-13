cfc186ad41b0c5bb8fee7abffc7578b3a0a314f1
Adrien van Driel <coenvand@gmail.com>
2019-01-13 15:54:40 +0100

    Initial project

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # DomainForwardingApp
-Simple domain forwarding app using ASP.NET Core that can forward one domain to another domain.
+Simple domain forwarding app using ASP.NET Core in combination with Azure Web Apps (can work on plain IIS as well) that can forward one domain to another domain.
+
+How to use:
+1. Add your domains to the domain register in Startup.cs
+2. Create an Azure Web App with Custom Domains, add the to-forward domains to your list of custom domains of the Azure Web App. 
+Add DNS records to enable this (see Azure Docs).
+3. Publish DomainForwardingApp to your Azure Web App.
+4. Incoming requests will be analyzed on hostname and forwarded if hostname exists in the domain list. 
+5. If you have a frequently changing list of domains, change the data source for the domain list from a dictionary to a dynamic source (e.g. database) if you want to avoid publishing the project every time you add/update a domain.
+
+Read my blogpost for a more detailed explanation at https://www.vandriel.me/forwarding-domain-names-with-aspnet-core-and-azure-web-apps
---
+++ src/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace DomainForwardingApp
+{
+    public class Startup
+    {
+        private static IDictionary<string, string> Domains { get; set; }
+
+        public Startup()
+        {
+            RegisterDomains();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.Run(context =>
+            {
+                string originDomain = context.Request.Host.Value;
+                bool isRegisteredDomain = TryGetTargetDomain(originDomain, out string targetDomain);
+                if (isRegisteredDomain)
+                {
+                    context.Response.Redirect(targetDomain, permanent: true);
+                }
+                else
+                {
+                    context.Response.StatusCode = 404;
+                }
+                return Task.FromResult<object>(null);
+            });
+        }
+
+        private static bool TryGetTargetDomain(string originDomain, out string targetDomain)
+        {
+            bool isRegisteredDomain;
+            if (Domains.ContainsKey(originDomain))
+            {
+                targetDomain = Domains[originDomain];
+                isRegisteredDomain = true;
+            }
+            else
+            {
+                targetDomain = null;
+                isRegisteredDomain = false;
+            }
+
+            return isRegisteredDomain;
+        }
+
+        private static void RegisterDomains()
+        {
+            const string mainDomain = "https://www.maindomain.com";
+
+            //Key: origin domain, Value: target domain
+            Domains = new Dictionary<string, string>();
+            Domains.Add("otherdomain.com", mainDomain);
+            Domains.Add("www.otherdomain.com", mainDomain);
+        }
+    }
+}
